6580409525f2db1946ca338006f243ccac39d0b1
Karina <androskarina@yandex.ru>
2026-02-04 14:56:42 +0300

    edit

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
+
 //import packages
-import "fmt" 
+import "fmt"
 
 //start program
 func main(){
-	//output text
+	//output text with new line
 	fmt.Println("Hello, mzfk")
+	
+	//output text without  new line
+	fmt.Print("Meow")
+
+	//variables
+	// score := 0
+	// score++ //increment
+	// score-- //decrement
+
+	var number int
+	var text string
+	fmt.Println("number:", number)
+	fmt.Println(text)
+	// fmt.Println(score)
+
+	//if else
+	score := 5
+	if score > 10{
+		fmt.Println("Good")
+	}else{
+		fmt.Println("Bad")
+	}
 }
